8224f1c5c55f794fa1131e3da2852b8ef90362fc
Nertsal <sasha.kudasov04@gmail.com>
2021-12-24 16:36:40 +0300

    Stick points at the mouse

--- src/game_state/stick.rs
+++ src/game_state/stick.rs
@@ -3,3 +3,9 @@ use super::*;
 pub struct Stick {
     pub position: Vec2<f32>,
 }
+
+impl Stick {
+    pub fn point_at(&mut self, target: Vec2<f32>) {
+        self.position = target - vec2(0.0, constants::STICK_SIZE.y / 2.0);
+    }
+}
--- src/game_state/update.rs
+++ src/game_state/update.rs
@@ -2,5 +2,11 @@ use super::*;
 
 impl GameState {
     pub fn update_impl(&mut self, delta_time: f32) {
+        // Move stick under the mouse
+        let mouse_pos = self.geng.window().mouse_pos().map(|x| x as f32);
+        let mouse_world_pos = self
+            .camera
+            .screen_to_world(self.framebuffer_size, mouse_pos);
+        self.stick.point_at(mouse_world_pos);
     }
 }
